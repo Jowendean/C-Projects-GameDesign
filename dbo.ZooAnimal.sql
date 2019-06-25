e54846ac370ab3ea74403e65780bcbbfa8765ecd
Jordan Ball <jowendean1@gmail.com>
2019-06-25 08:23:14 -0500

    Delete dbo.ZooAnimal.sql

--- dbo.ZooAnimal.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ZooAnimal] (
-    [Id]       INT IDENTITY (1, 1) NOT NULL,
-    [ZooID]    INT NOT NULL,
-    [AnimalID] INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [AnimalFK] FOREIGN KEY ([AnimalID]) REFERENCES [dbo].[Animal] ([Id]) ON DELETE CASCADE,
-    CONSTRAINT [ZooFK] FOREIGN KEY ([ZooID]) REFERENCES [dbo].[Zoo] ([Id]) ON DELETE CASCADE
-);
-
